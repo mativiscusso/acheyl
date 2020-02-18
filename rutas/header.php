e7202a7eca9f9ea035b4072c0b4702cd2a50b8e7
mativiscusso <mativiscusso@gmail.com>
2020-02-18 19:24:31 -0300

    cambios

--- rutas/header.php
+++ rutas/header.php
@@ -10,7 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/8de40d40f6.js" crossorigin="anonymous"></script>
-    <link href="../css/estilos.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
     <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
@@ -19,42 +19,40 @@
 </head>
 
 <body>
-<header class="sticky-top">
-        <nav id="barraMenu" class="navbar navbar-expand-lg navbar-light p-2 mb-3 bg-white rounded">
-            <a id="logo" class="navbar-brand animated pulse delay-1s" href="index2"><img class="logo img-fluid" src="img/acheyl_logo.png" alt=""></a>
+<nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href=""><img id="logo" src="img/acheyl_logo2.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <svg class="ham hamRotate ham1" viewBox="0 0 100 100" width="60"
+                    onclick="this.classList.toggle('active')">
+                    <path class="line top"
+                        d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
+                    <path class="line middle" d="m 30,50 h 40" />
+                    <path class="line bottom"
+                        d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
+                </svg>
             </button>
 
-            <div class="collapse navbar-collapse text-body" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-<!--                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            SERVICIOS
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="rutas/kine">KINESIOLOGIA</a>
-                            <a class="dropdown-item" href="rutas/estetica">ESTETICA</a>
-                        </div>
-                    </li> -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="../rutas/espmedicas">PRODUCTOS</a>
+                        <a class="nav-link" href="#">QUIENES SOMOS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../rutas/promociones">SERVICIOS</a>
+                        <a class="nav-link" href="#">SERVICIOS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../rutas/faqs">FRANQUICIAS</a>
+                        <a class="nav-link" href="#">PRODUCTOS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../rutas/faqs">FAQs</a>
+                        <a class="nav-link" href="#">FRANQUICIAS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../rutas/contacto">CONTACTO</a>
+                        <a class="nav-link" href="#">FAQs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTACTO</a>
                     </li>
                 </ul>
             </div>
         </nav>
-    </header>
--- rutas/productos.php
+++ rutas/productos.php
@@ -2,7 +2,116 @@
 require_once('header.php');
 ?>
 
+<div class="my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-3">
+                <div class="card">
+                    <div class="card__image-container">
+                        <img class="card__image" src="../img/productos.png" alt="">
+                    </div>
 
+                    <svg class="card__svg" viewBox="0 0 800 500">
+
+                        <path
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
+                            stroke="transparent" fill="#333" />
+                        <path class="card__line"
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
+                            stroke="pink" stroke-width="3" fill="transparent" />
+                    </svg>
+
+                    <div class="card__content">
+                        <h1 class="card__title">Lorem ipsum</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod
+                            autem
+                            omnis, amet eaque unde perspiciatis adipisci possimus quam facere illo et quisquam quia
+                            earum
+                            nesciunt porro.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-3">
+                <div class="card">
+                    <div class="card__image-container">
+                        <img class="card__image" src="../img/productos.png" alt="">
+                    </div>
+
+                    <svg class="card__svg" viewBox="0 0 800 500">
+
+                        <path
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
+                            stroke="transparent" fill="#333" />
+                        <path class="card__line"
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
+                            stroke="pink" stroke-width="3" fill="transparent" />
+                    </svg>
+
+                    <div class="card__content">
+                        <h1 class="card__title">Lorem ipsum</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod
+                            autem
+                            omnis, amet eaque unde perspiciatis adipisci possimus quam facere illo et quisquam quia
+                            earum
+                            nesciunt porro.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-3">
+                <div class="card">
+                    <div class="card__image-container">
+                        <img class="card__image" src="../img/productos.png" alt="">
+                    </div>
+
+                    <svg class="card__svg" viewBox="0 0 800 500">
+
+                        <path
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
+                            stroke="transparent" fill="#333" />
+                        <path class="card__line"
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
+                            stroke="pink" stroke-width="3" fill="transparent" />
+                    </svg>
+
+                    <div class="card__content">
+                        <h1 class="card__title">Lorem ipsum</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod
+                            autem
+                            omnis, amet eaque unde perspiciatis adipisci possimus quam facere illo et quisquam quia
+                            earum
+                            nesciunt porro.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-3">
+                <div class="card">
+                    <div class="card__image-container">
+                        <img class="card__image" src="../img/productos.png" alt="">
+                    </div>
+
+                    <svg class="card__svg" viewBox="0 0 800 500">
+
+                        <path
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
+                            stroke="transparent" fill="#333" />
+                        <path class="card__line"
+                            d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
+                            stroke="pink" stroke-width="3" fill="transparent" />
+                    </svg>
+
+                    <div class="card__content">
+                        <h1 class="card__title">Lorem ipsum</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod
+                            autem
+                            omnis, amet eaque unde perspiciatis adipisci possimus quam facere illo et quisquam quia
+                            earum
+                            nesciunt porro.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 require_once('footer.php');

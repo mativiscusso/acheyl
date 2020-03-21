16c4c7e31b8b346da8af2c89b518ff14fc2adad7
mativiscusso <mativiscusso@gmail.com>
2020-03-21 15:44:02 -0300

    carouselAromatizacion

--- rutas/aromatizacion.php
+++ rutas/aromatizacion.php
@@ -2,100 +2,87 @@
 require_once('header.php');
 ?>
 
-        <h2 class="text-center py-5">AROMATIZACION</h2>
-        <div class="row">
-            <div class="col-12 col-sm-12 col-md-6">
-            <div class="sliderAroma">
-                <ul>
-                    <li>
-                        <img src="../img/arom1.jpg" alt="">
-                    </li>
-                    <li>
-                        <img src="../img/arom2.jpg" alt="">
-                    </li>
-                    <li>
-                        <img src="../img/arom3.jpg" alt="">
-                    </li>
-                </ul>
-            </div>
-            </div>
-            <div id="textoAroma"class="col-12 col-sm-12 col-md-6 px-3">
-            <p><i>"Ofreceles a tus clientes, una experiencia diferente"</i></p>
-            <p>SERVICIO DE AROMATIZACIÓN DE AMBIENTES</p> 
-            <p>DESODORIZANTE - AROMATIZANTE - BACTERICIDA</p>
-                <ul >
-                    <li>Cada aparato cubre aprox una superficie de 40 m2.</li>
-                    <li>Instalación sin cargo</li>
-                    <li>Mantenimiento cada 15 días (Renovación de fragancias)</li>
-                    <li>Prueba gratuita por 48 horas</li>
-                    <li>Amplia variedad de fragancias</li>
-                </ul>
-            <span id="spanAroma">“CON UN ABONO MENSUAL, MANTENÉ TUS AMBIENTES AROMATIZADOS LAS 24 HORAS”</span><br>
-            <button type="button" class="btn btn-dark my-5">QUIERO PROBARLO GRATIS POR 48hs</button>
-            </div>
-        </div>
-
-
-    <div data-aos="flip-right" class="my-5" id="banner2">
-        <p>AMPLIA VARIEDAD EN FRAGANCIAS</p>
-        <div class="slider">
-            <div class="slide-track">
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
+<h2 class="text-center py-5">AROMATIZACION</h2>
+<div class="row">
+    <div class="col-12 col-sm-12 col-md-6">
+        <div id="carouselAroma" data-interval="4000" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="../img/arom1.jpg" alt="First slide">
                 </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="../img/arom2.jpg" alt="Second slide">
                 </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
-                </div>
-                <div class="slide">
-                    <img src="../img/fragancia.png" height="100" width="50"
-                        alt="" />
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="../img/arom3.jpg" alt="Third slide">
                 </div>
             </div>
         </div>
-        <button type="button" class="btn btn-dark my-5">QUIERO CONTRATAR</button>
     </div>
+    <div id="textoAroma" class="col-12 col-sm-12 col-md-6 px-3">
+        <p><i>"Ofreceles a tus clientes, una experiencia diferente"</i></p>
+        <p>SERVICIO DE AROMATIZACIÓN DE AMBIENTES</p>
+        <p>DESODORIZANTE - AROMATIZANTE - BACTERICIDA</p>
+        <ul>
+            <li>Cada aparato cubre aprox una superficie de 40 m2.</li>
+            <li>Instalación sin cargo</li>
+            <li>Mantenimiento cada 15 días (Renovación de fragancias)</li>
+            <li>Prueba gratuita por 48 horas</li>
+            <li>Amplia variedad de fragancias</li>
+        </ul>
+        <span id="spanAroma">“CON UN ABONO MENSUAL, MANTENÉ TUS AMBIENTES AROMATIZADOS LAS 24 HORAS”</span><br>
+        <button type="button" class="btn btn-dark my-5">QUIERO PROBARLO GRATIS POR 48hs</button>
+    </div>
+</div>
+
+
+<div data-aos="flip-right" class="my-5" id="banner2">
+    <p>AMPLIA VARIEDAD EN FRAGANCIAS</p>
+    <div class="slider">
+        <div class="slide-track">
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../img/fragancia.png" height="100" width="50" alt="" />
+            </div>
+        </div>
+    </div>
+    <button type="button" class="btn btn-dark my-5">QUIERO CONTRATAR</button>
+</div>
 
 
 

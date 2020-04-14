fc1f40bcdf0f7e4c9cfeeafdf671ac1fe45da6f2
mativiscusso <mativiscusso@gmail.com>
2020-04-14 16:40:16 -0300

    script request async

--- rutas/footer.php
+++ rutas/footer.php
@@ -74,42 +74,42 @@
 
 </footer>
 
-<script src="../js/navbar.js"></script>
-<script src="../js/top.js"></script>
-<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-<script>
+<script async src="../js/navbar.js"></script>
+<script async src="../js/top.js"></script>
+<script async src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script async>
     AOS.init();
 </script>
 
-<script>
+<script async>
     const scroll = new SmoothScroll('a[href*="#"]', {
         speed: 500
     });
 </script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    <script async src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    <script async src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    <script async src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
-    <script src="https://kit.fontawesome.com/8de40d40f6.js" crossorigin="anonymous"></script>
+    <script async src="https://kit.fontawesome.com/8de40d40f6.js" crossorigin="anonymous"></script>
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
     <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script async src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
-    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <script async src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
     <!-- CSS -->
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-    <script src="../js/mail.js"></script>
+    <script async src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+    <script async src="../js/mail.js"></script>
 </body>
 
 </html>

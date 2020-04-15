f599edc5e6bcfbd6f62c4a1e32afff135f5ad1e4
mativiscusso <mativiscusso@gmail.com>
2020-04-15 11:52:10 -0300

    ultima version

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,1 @@
-function stickyElement(e) {
-  var navbar = document.querySelector(".navbar.navbar-expand-lg.navbar-light");
-  var scrollValue = window.scrollY;
-
-  if (scrollValue > 150) {
-    navbar.classList.add("is-fixed");
-  } else if (scrollValue < 30) {
-    navbar.classList.remove("is-fixed");
-  }
-}
-
-window.addEventListener("scroll", stickyElement);
+function stickyElement(e){var navbar=document.querySelector(".navbar.navbar-expand-lg.navbar-light"),scrollValue=window.scrollY;scrollValue>150?navbar.classList.add("is-fixed"):scrollValue<30&&navbar.classList.remove("is-fixed")}window.addEventListener("scroll",stickyElement);
--- rutas/contacto.php
+++ rutas/contacto.php
@@ -1,9 +1,10 @@
 <?php
-    $toEmail = "mativiscusso@gmail.com";
+    $toEmail = "contacto@acheyl.com.ar";
     $mailHeaders = "From: " . $_POST["nombre"] . "<". $_POST["email"] .">\r\n";
     if(mail($toEmail, $_POST["mensaje"], $mailHeaders)) {
-        print "<p class='success'>Mail Sent.</p>";
-    } else {
+        print "<p class='success'>Envio Exitoso</p>";
+        header('Refresh: 1; URL=http://acheyl.com.ar');
+        } else {
         print "<p class='Error'>Problem in Sending Mail.</p>";
     }
 ?>

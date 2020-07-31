634a0b527b9b5254055bb874a184648451b2c323
mativiscusso <mativiscusso@gmail.com>
2020-07-31 12:26:15 -0300

    nuevos cambios en productosy faqs

--- rutas/faqs.php
+++ rutas/faqs.php
@@ -1,171 +1,233 @@
-<?php
-require_once('header.php');
-?>
-<div class="container my-5">
-
-  <h1 class="text-center">Preguntas Frecuentes</h1>
-  <div id="faqs" class="container my-3">
-    <div class="accordion" id="accordionExample">
-      <div class="cards">
-        <div class="card-header" id="headingOne">
-          <h2 class="mb-0">
-            <button id="color" class="btn btn-link text-dark font-weight-bold" type="button" data-toggle="collapse"
-              data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-              ¿Qué es el servicio de aromatización?
-            </button>
-          </h2>
-        </div>
-
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-          <div class="card-body">
-            Este producto garantiza la difusión automática de compuestos aromáticos para anular los malos olores de
-            ambientes enrarecidos en especial, con humo de cigarrillo. Ofrecemos perfumes ambientales con aromas
-            Naturales, ecológicas con notas frescas y acorde a sensación a limpio. Reúnen tres propiedades necesarias en
-            una misma aplicación:
-            <ol>
-              <li>Desodorizante: anula en su faz primaria los malos olores.</li>
-              <li>Aromatizante: luego de la acción anterior para aromatizar.</li>
-              <li>Bactericida: neutraliza las bacterias y agentes que producen los malos olores procurando una desinfección
-            de ambiente.</li>
-            </ol>
-            Estas composiciones aromáticas son comprobadamente inofensivas, no tóxicas, no irritantes.
-          </div>
-        </div>
-      </div>
-      <div class="cards">
-        <div class="card-header" id="headingTwo">
-          <h2 class="mb-0">
-            <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-              data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              ¿De qué material es el dispenser y cómo funciona?
-            </button>
-          </h2>
-        </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-          <div class="card-body">
-            El dispenser es de material plástico , de forma circular, funciona a 220vt y consume sólo 2 wts.
-            Éste producto es un equipo de potencia el cual trabaja las 24 Hs teniendo un timer automático de encendido y
-            corte, siendo de bajo consumo (12 Volts), con una cobertura máxima de 50 m2 por equipos en área limpias
-            (libres de malos olores) y 25 m2 en ambientes contaminados. Este sistema garantiza fragancias continuas las
-            24 hs del día los 365 del año
-          </div>
-        </div>
-        <div class="cards">
-          <div class="card-header" id="headingThree">
-            <h2 class="mb-0">
-              <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-                data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                ¿Con qué frecuencia se deben recargar las fragancias?
-              </button>
-            </h2>
-          </div>
-          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-            <div class="card-body">
-              La recarga de las esencias es con frecuencia quincenal. Nuestro personal hará una visita cada 15 días a su
-              establecimiento para un mantenimiento del servicio, renovación y/o cambio de fragancias y asesoramiento.
-            </div>
-          </div>
-        </div>
-        <div class="cards">
-          <div class="card-header" id="headingFour">
-            <h2 class="mb-0">
-              <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-                data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                ¿Si adquiero el servicio de Aromatización, el dispenser es mío?
-              </button>
-            </h2>
-          </div>
-          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-            <div class="card-body">
-              Para formalizar la operación la empresa Ache&L realiza un comodato donde se le aclara al cliente que los
-              dispensadores de ambiente son de propiedad de la empresa.- Lo que el cliente adquiere a cambio de su abono
-              mensual es el servicio de aromatización, con mantenimiento quincenal de fragancias para poder tener sus
-              ambientes aromatizados las 24 horas del día los 365 días del año.
-            </div>
-          </div>
-        </div>
-        <div class="cards">
-          <div class="card-header" id="headingFour">
-            <h2 class="mb-0">
-              <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-                data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                ¿Si adquiero el servicio de Aromatización, el dispenser es mío?
-              </button>
-            </h2>
-          </div>
-          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-            <div class="card-body">
-              Para formalizar la operación la empresa Ache&L realiza un comodato donde se le aclara al cliente que los
-              dispensadores de ambiente son de propiedad de la empresa.- Lo que el cliente adquiere a cambio de su abono
-              mensual es el servicio de aromatización, con mantenimiento quincenal de fragancias para poder tener sus
-              ambientes aromatizados las 24 horas del día los 365 días del año.
-            </div>
-          </div>
-        </div>
-        <div class="cards">
-          <div class="card-header" id="headingFour">
-            <h2 class="mb-0">
-              <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-                data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                Puedo probarlo durante 1 mes y dejarlo?
-              </button>
-            </h2>
-          </div>
-          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-            <div class="card-body">
-            Solicitamos una permanencia mínima en el servicio de 6 meses. Si te damos la posibilidad que lo pruebes en forma gratuita y sin obligación de compra durante 48 horas.
-            </div>
-          </div>
-        </div>
-        <div class="cards">
-          <div class="card-header" id="headingFour">
-            <h2 class="mb-0">
-              <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-                data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                Puedo probarlo durante 1 mes y dejarlo?
-              </button>
-            </h2>
-          </div>
-          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-            <div class="card-body">
-            Solicitamos una permanencia mínima en el servicio de 6 meses. Si te damos la posibilidad que lo pruebes en forma gratuita y sin obligación de compra durante 48 horas.
-            </div>
-          </div>
-        </div>
-        <div class="cards">
-          <div class="card-header" id="headingFour">
-            <h2 class="mb-0">
-              <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-                data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                Puedo probarlo durante 1 mes y dejarlo?
-              </button>
-            </h2>
-          </div>
-          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-            <div class="card-body">
-            Solicitamos una permanencia mínima en el servicio de 6 meses. Si te damos la posibilidad que lo pruebes en forma gratuita y sin obligación de compra durante 48 horas.
-            </div>
-          </div>
-        </div>
-        <div class="cards">
-          <div class="card-header" id="headingFour">
-            <h2 class="mb-0">
-              <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button"
-                data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                ¿Cuales son las fragancias mas utilizadas?
-              </button>
-            </h2>
-          </div>
-          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-            <div class="card-body">
-            Kosiuko, Frutilla, Chicle, Frutos rojos, Coco, La vida es bella, Limón, Lavanda suave, Coniglio, Vainilla. 
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <?php
-require_once('footer.php');
+<?php
+
+require_once('header.php');
+
 ?>
+
+<div class="container my-5">
+
+
+
+   <h1 class="text-center">Preguntas Frecuentes</h1>
+
+   <div id="faqs" class="container my-3">
+
+      <div class="accordion" id="accordionExample">
+
+         <div class="cards">
+
+            <div class="card-header" id="headingOne">
+
+               <h2 class="mb-0">
+
+                  <button id="color" class="btn btn-link text-dark font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+
+                     ¿Qué es el servicio de aromatización?
+
+                  </button>
+
+               </h2>
+
+            </div>
+
+
+
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+
+               <div class="card-body">
+
+                  Este producto garantiza la difusión automática de compuestos aromáticos para anular los malos olores de
+
+                  ambientes enrarecidos en especial, con humo de cigarrillo. Ofrecemos perfumes ambientales con aromas
+
+                  Naturales, ecológicas con notas frescas y acorde a sensación a limpio. Reúnen tres propiedades necesarias en
+
+                  una misma aplicación:
+
+                  <ol>
+
+                     <li>Desodorizante: anula en su faz primaria los malos olores.</li>
+
+                     <li>Aromatizante: luego de la acción anterior para aromatizar.</li>
+
+                     <li>Bactericida: neutraliza las bacterias y agentes que producen los malos olores procurando una desinfección
+
+                        de ambiente.</li>
+
+                  </ol>
+
+                  Estas composiciones aromáticas son comprobadamente inofensivas, no tóxicas, no irritantes.
+
+               </div>
+
+            </div>
+
+         </div>
+
+         <div class="cards">
+
+            <div class="card-header" id="headingTwo">
+
+               <h2 class="mb-0">
+
+                  <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+
+                     ¿De qué material es el dispenser y cómo funciona?
+
+                  </button>
+
+               </h2>
+
+            </div>
+
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+
+               <div class="card-body">
+
+                  El dispenser es de material plástico , de forma circular, funciona a 220vt y consume sólo 2 wts.
+
+                  Éste producto es un equipo de potencia el cual trabaja las 24 Hs teniendo un timer automático de encendido y
+
+                  corte, siendo de bajo consumo (12 Volts), con una cobertura máxima de 50 m2 por equipos en área limpias
+
+                  (libres de malos olores) y 25 m2 en ambientes contaminados. Este sistema garantiza fragancias continuas las
+
+                  24 hs del día los 365 del año
+
+               </div>
+
+            </div>
+
+            <div class="cards">
+
+               <div class="card-header" id="headingThree">
+
+                  <h2 class="mb-0">
+
+                     <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+
+                        ¿Con qué frecuencia se deben recargar las fragancias?
+
+                     </button>
+
+                  </h2>
+
+               </div>
+
+               <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+
+                  <div class="card-body">
+
+                     La recarga de las esencias es con frecuencia quincenal. Nuestro personal hará una visita cada 15 días a su
+
+                     establecimiento para un mantenimiento del servicio, renovación y/o cambio de fragancias y asesoramiento.
+
+                  </div>
+
+               </div>
+
+            </div>
+
+            <div class="cards">
+
+               <div class="card-header" id="headingFour">
+
+                  <h2 class="mb-0">
+
+                     <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+
+                        ¿Si adquiero el servicio de Aromatización, el dispenser es mío?
+
+                     </button>
+
+                  </h2>
+
+               </div>
+
+               <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+
+                  <div class="card-body">
+
+                     Para formalizar la operación la empresa Ache&L realiza un comodato donde se le aclara al cliente que los
+
+                     dispensadores de ambiente son de propiedad de la empresa.- Lo que el cliente adquiere a cambio de su abono
+
+                     mensual es el servicio de aromatización, con mantenimiento quincenal de fragancias para poder tener sus
+
+                     ambientes aromatizados las 24 horas del día los 365 días del año.
+
+                  </div>
+
+               </div>
+
+            </div>
+
+            <div class="cards">
+
+               <div class="card-header" id="headingFour">
+
+                  <h2 class="mb-0">
+
+                     <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+
+                        Puedo probarlo durante 1 mes y dejarlo?
+
+                     </button>
+
+                  </h2>
+
+               </div>
+
+               <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+
+                  <div class="card-body">
+
+                     Solicitamos una permanencia mínima en el servicio de 6 meses. Si te damos la posibilidad que lo pruebes en forma gratuita y sin obligación de compra durante 48 horas.
+
+                  </div>
+
+               </div>
+
+            </div>
+
+            <div class="cards">
+
+               <div class="card-header" id="headingFour">
+
+                  <h2 class="mb-0">
+
+                     <button id="color" class="btn btn-link text-dark font-weight-bold collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+
+                        ¿Cuales son las fragancias mas utilizadas?
+
+                     </button>
+
+                  </h2>
+
+               </div>
+
+               <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+
+                  <div class="card-body">
+
+                     Kosiuko, Frutilla, Chicle, Frutos rojos, Coco, La vida es bella, Limón, Lavanda suave, Coniglio, Vainilla.
+
+                  </div>
+
+               </div>
+
+            </div>
+
+         </div>
+
+      </div>
+
+   </div>
+
+</div>
+
+   <?php
+
+   require_once('footer.php');
+
+   ?>
--- rutas/productos.php
+++ rutas/productos.php
@@ -13,6 +13,71 @@ require_once('header.php');
     <!-- Flickity HTML init -->
 
     <div class="carousel" data-flickity='{ "wrapAround": true }'>
+        <div id="carouselProducto" class="carousel-cell">
+
+            <div class="card" style="width: 18rem;text-align:center;">
+
+                <img class="card-img-top" src="../img/productos/web_electrico.jpg" alt="Card image cap">
+
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between; ">
+
+                    <h5 class="card-title">AROMATIZADOR ELÉCTRICO</h5>
+
+                    <p class="card-text">Insumo: Esencias para electro</p>
+
+
+
+                    <a href="https://wa.link/8z1xsz" class="btn btn-dark" onclick="gtag('event', 'enlace', { 'event_category': 'contacto', 'event_action': 'comprarprod', 'event_label': 'click', 'value': '0'});">COMPRAR</a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="carouselProducto" class="carousel-cell">
+
+            <div class="card" style="width: 18rem;text-align:center;">
+
+                <img class="card-img-top" src="../img/productos/web_fragancia.jpg" alt="Card image cap">
+
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between; ">
+
+                    <h5 class="card-title">ESENCIAS PARA ELÉCTRICOS</h5>
+
+                    <p class="card-text">Amplia variedad de fragancias</p>
+
+
+
+                    <a href="https://wa.link/8z1xsz" class="btn btn-dark" onclick="gtag('event', 'enlace', { 'event_category': 'contacto', 'event_action': 'comprarprod', 'event_label': 'click', 'value': '0'});">COMPRAR</a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="carouselProducto" class="carousel-cell">
+
+            <div class="card" style="width: 18rem;text-align:center;">
+
+                <img class="card-img-top" src="../img/productos/dispenser_higienico.png" alt="Card image cap">
+
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
+
+                    <h5 class="card-title">DISPENSER PAPEL HIGIENICO</h5>
+
+                    <p class="card-text">Acero inoxidable o plástico</p>
+
+
+
+                    <a href="https://wa.link/8z1xsz" class="btn btn-dark" onclick="gtag('event', 'enlace', { 'event_category': 'contacto', 'event_action': 'comprarprod', 'event_label': 'click', 'value': '0'});">COMPRAR</a>
+
+                </div>
+
+            </div>
+
+        </div>
 
         <div id="carouselProducto" class="carousel-cell">
 
@@ -20,9 +85,7 @@ require_once('header.php');
 
                 <img class="card-img-top" src="../img/productos/dispenser_jabon.png" alt="Card image cap">
 
-                <div class="card-body"
-
-                    style="display:flex; flex-direction:column;height:30vh;justify-content: space-between; ">
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between; ">
 
                     <h5 class="card-title">DISPENSER JABON LIQUIDO</h5>
 
@@ -42,11 +105,29 @@ require_once('header.php');
 
             <div class="card" style="width: 18rem;text-align:center;">
 
+                <img class="card-img-top" src="../img/productos/insumos.png" alt="Card image cap">
+
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
+
+                    <h5 class="card-title">INSUMOS</h5>
+
+                    <p class="card-text">Alcohol en Gel, Jabon Liquido, Papel, Toallas, Fragancias</p>
+
+                    <a href="https://wa.link/8z1xsz" class="btn btn-dark" onclick="gtag('event', 'enlace', { 'event_category': 'contacto', 'event_action': 'comprarprod', 'event_label': 'click', 'value': '0'});">COMPRAR</a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="carouselProducto" class="carousel-cell">
+
+            <div class="card" style="width: 18rem;text-align:center;">
+
                 <img class="card-img-top" src="../img/productos/dispenser_toallas.png" alt="Card image cap">
 
-                <div class="card-body"
-
-                    style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
 
                     <h5 class="card-title">DISPENSER TOALLA</h5>
 
@@ -68,9 +149,7 @@ require_once('header.php');
 
                 <img class="card-img-top" src="../img/productos/dispenser_toalla_ind.png" alt="Card image cap">
 
-                <div class="card-body"
-
-                    style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
 
                     <h5 class="card-title">DISPENSER TOALLA CON PALANCA</h5>
 
@@ -90,57 +169,9 @@ require_once('header.php');
 
             <div class="card" style="width: 18rem;text-align:center;">
 
-                <img class="card-img-top" src="../img/productos/insumos.png" alt="Card image cap">
-
-                <div class="card-body"
-
-                    style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
-
-                    <h5 class="card-title">INSUMOS</h5>
-
-                    <p class="card-text">Alcohol en Gel, Jabon Liquido, Papel, Toallas, Fragancias</p>
-
-                    <a href="https://wa.link/8z1xsz" class="btn btn-dark" onclick="gtag('event', 'enlace', { 'event_category': 'contacto', 'event_action': 'comprarprod', 'event_label': 'click', 'value': '0'});">COMPRAR</a>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <div id="carouselProducto" class="carousel-cell">
-
-            <div class="card" style="width: 18rem;text-align:center;">
-
-                <img class="card-img-top" src="../img/productos/dispenser_higienico.png" alt="Card image cap">
-
-                <div class="card-body"
-
-                    style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
-
-                    <h5 class="card-title">DISPENSER PAPEL HIGIENICO</h5>
-
-                    <p class="card-text">Acero inoxidable o plástico</p>
-
-
-
-                    <a href="https://wa.link/8z1xsz" class="btn btn-dark" onclick="gtag('event', 'enlace', { 'event_category': 'contacto', 'event_action': 'comprarprod', 'event_label': 'click', 'value': '0'});">COMPRAR</a>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <div id="carouselProducto" class="carousel-cell">
-
-            <div class="card" style="width: 18rem;text-align:center;">
-
                 <img class="card-img-top" src="../img/productos/dispenser_bobina.png" alt="Card image cap">
 
-                <div class="card-body"
-
-                    style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
+                <div class="card-body" style="display:flex; flex-direction:column;height:30vh;justify-content: space-between">
 
                     <h5 class="card-title">DISPENSER BOBINA 5kg</h5>
 
